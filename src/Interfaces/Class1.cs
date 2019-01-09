6d00921af6f505295cc4899f19194d7708df758c
Maciej Walanus <mwalanus@outlook.com>
2019-01-09 15:27:12 +0100

    Scaffolding

---
+++ src/Interfaces/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Interfaces
+{
+    public class Token
+    {
+        public string Word { get; set; }
+        /// <summary>
+        /// Count/1000000
+        /// </summary>
+        public int Frequency { get; set; }
+    }
+
+    public enum Lanugage
+    {
+        English,
+        Polish,
+        //......
+    }
+
+/// <summary>
+/// Słownik dla pojedynczego języka
+/// </summary>
+public class LanguageDictionary
+{
+        //
+        ///
+        public Language Langauge { get; set; }
+
+        private Dictionary<string, int> _internal;
+    }
+
+    public class LanguagesMap
+    {
+
+    }
+
+
+    public interface IParser
+    {
+
+    }
+}
