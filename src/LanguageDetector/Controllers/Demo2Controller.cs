933e04f371f26dc0d82991f00f607ac802e30a51
Maciej Walanus <mwalanus@outlook.com>
2019-01-17 23:24:44 +0100

    Better Interface

--- src/LanguageDetector/Controllers/Demo2Controller.cs
+++ src/LanguageDetector/Controllers/Demo2Controller.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Linq;
 using Analizers;
 using GoogleTranslateApiClient;
+using Interfaces;
 using PageService;
 
 namespace LanguageDetector.Controllers
@@ -18,7 +20,6 @@ namespace LanguageDetector.Controllers
 
         public void Run(string basePath)
         {
-
             var task = _newsArticleProvider.GetNextArticleAsync();
             task.Wait();
             var article = task.Result;
@@ -26,15 +27,32 @@ namespace LanguageDetector.Controllers
             var alg1 = new Alg1Analizer(basePath);
             var alg2 = new Alg2Analizer(basePath);
 
-            var analysis1 = alg1.Analize(article.Content).GetDiscoveredLanguage();
-            var analysis2 = alg2.Analize(article.Content).GetDiscoveredLanguage();
+            var analysis1 = alg1.Analize(article.Content);
+            var analysis2 = alg2.Analize(article.Content);
             var googleDetected = _translateServiceClient.DetectLanguage(article.Content);
 
-
+            Console.WriteLine("Article content:");
+            Console.WriteLine(article.Content);
+            Console.WriteLine("-----------------------------------------------");
             Console.WriteLine($"Actual lng: {article.ActualLanguage}");
-            Console.WriteLine($"Alg1 lng: {analysis1}");
-            Console.WriteLine($"Alg2 lng: {analysis2}");
+            Console.WriteLine("Alg1 analysis:");
+            PrintFullAnalysis(analysis1);
+            Console.WriteLine("Alg2 analysis:");
+            PrintFullAnalysis(analysis2);
             Console.WriteLine($"Google lng: {googleDetected}");
+            Console.WriteLine("-----------------------------------------------");
+        }
+
+        private void PrintFullAnalysis(Analysis analysis)
+        {
+            var language = analysis.GetDiscoveredLanguage();
+            Console.WriteLine($"Detected language: {language}");
+            Console.WriteLine("-------");
+            foreach (var entry in analysis.analysisMap.OrderByDescending(e=>e.Value))
+            {
+                Console.WriteLine($"Language: {entry.Key}, with confidence: {entry.Value}");
+            }
+            Console.WriteLine("-------");
         }
     }
 }
