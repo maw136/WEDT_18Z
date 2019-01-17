1fe5d3a47b0a5679c145803edc3dbddfd563b11e
Maciej Walanus <mwalanus@outlook.com>
2019-01-17 02:17:17 +0100

    Working sample

--- src/LanguageDetector/Controllers/Demo2Controller.cs
+++ src/LanguageDetector/Controllers/Demo2Controller.cs
@@ -28,9 +28,15 @@ namespace LanguageDetector.Controllers
             var alg1 = new Alg1Analizer(basePath);
             var alg2 = new Alg2Analizer(basePath);
 
-            var analysis1 = alg1.Analize(article.Content);
-            var analysis2 = alg2.Analize(article.Content);
-            _translateServiceClient.DetectLanguage(article.Content);
+            var analysis1 = alg1.Analize(article.Content).GetDiscoveredLanguage();
+            var analysis2 = alg2.Analize(article.Content).GetDiscoveredLanguage();
+            var googleDetected = _translateServiceClient.DetectLanguage(article.Content);
+
+
+            Console.WriteLine($"Actual lng: {article.ActualLanguage}");
+            Console.WriteLine($"Alg1 lng: {analysis1}");
+            Console.WriteLine($"Alg2 lng: {analysis2}");
+            Console.WriteLine($"Google lng: {googleDetected}");
         }
     }
 }
